c185de3d310c5e855f3307f09f8879a08a6c5bbf
Roshan Cafle <coderoshan@gmail.com>
2021-02-11 10:58:56 +0545

    Task 2 Begin

--- App.js
+++ App.js
@@ -5,25 +5,7 @@ import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.view1}>
-        <AntDesign name="MessageOutlined " size={32} color="white" />
-        <Text style={styles.text}> Messages!!!!!</Text>
-      </View>
-      <View style={styles.view2}>
-        <Text style={styles.text1}>Message 1</Text>
-        <Text style={styles.text1}>Message 2</Text>
-        <Text style={styles.text1}>Message 3</Text>
-        <Text style={styles.text1}>Message 4</Text>
-        <Text style={styles.text1}>Message 5</Text>
-      </View>
-      <View style={styles.view3}>
-        <Text>A</Text>
-        <Text>B</Text>
-        <Text>C</Text>
-        <Text>D</Text>
-      </View>
-    </View>
+    
   );
 }
 
@@ -34,34 +16,5 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  view1: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: "red",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  view2: {
-    flex: 10,
-    backgroundColor: "black",
-    alignItems: "flex-end",
-    justifyContent: "flex-end",
-  },
-  view3: {
-    flex: 2,
-    backgroundColor: "yellow",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "flex-end",
-  },
-  text: {
-    color: "white",
-    fontfamily: "Times New Roman",
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-  text1: {
-    color: "white",
-    fontSize: 29,
-  },
+  
 });
